1edc10230e6f85f0bdc3028990ba3af9d1d0fe21
Rameshwar Jadhav <rameshwarjadhav@Rameshwars-MacBook-Pro.local>
2017-06-23 00:38:29 +0530

    Initial commit of python basic concepts

---
+++ basics.py
@@ -0,0 +1,31 @@
+grocery_list = ['one', 'two', 'three', 'four', 'five']
+
+print('list item = ', grocery_list[0])
+
+grocery_list[0] = 'ONE'
+print('list item = ', grocery_list[0])
+
+# if/else clause
+
+name = 'Mishra'
+if name == 'John':
+    print('This is John, he is an artist')
+elif name == 'Rahul':
+    print('This is Rahul, he is a iOS programmer!')
+elif name == 'Mishra':
+    print('This is Mishra, he is java programmer!')
+
+# Dictionary:
+
+name_dict = {
+    "Apple" : "Mac, iPhone, ipad",
+    "Google" : "Android",
+    "Samsung" : "Galaxy"
+}
+
+print(name_dict["Apple"])
+
+
+
+
+
